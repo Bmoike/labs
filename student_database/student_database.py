2d5f310ddd6a74e12d7e00e79fcc07c0b76d808d
Michael Kerr <kerrjaxm@gmail.com>
2024-08-20 17:34:21 -0400

    Added search student by name feature

--- student_database/student_database.py
+++ student_database/student_database.py
@@ -1,4 +1,4 @@
-# how can I make this code neater?
+# how can I make this code neater? Are there too many functions?
 
 # list of student info
 names = ["sandy cheeks", "Squidward tennisball", "patrick Star", "sheldon plankton"]
@@ -6,6 +6,7 @@ hometown = ["dallas", "Easter Island", "Rock city", "Chum Bucket"]
 favorite_food = ["walnuts", "kelp", "krabby patties", "holograms"]
 
 
+# asks user if they would like to see the list of students
 def see_list():
     while True:
         total = input("Would you like to see the list of students?(yes/no): ")
@@ -24,44 +25,52 @@ def see_list():
 # validation of users student choice
 def validate_student():
     while True:
-        student = input("Please choose a student number. (1-4): ")
-        try:
-            student = int(student)
-            if student.is_integer() and 0 < student < len(names) + 1:
-                return student
-            else:
-                print("Please choose an integer from 1-4 only.")
-                continue
-        except ValueError:
-            print("Please choose an integer from 1-4 only.")
+        # gets to choose to find a student with their name or with their number
+        name_v_num = input("Would you like to search for a student with their name or their number?"
+                           "Type in either name or number for your choice: ")
+        name_v_num = name_v_num.lower()
+        # search by name then returns the index of the student
+        if name_v_num == "name":
+            stu_name = input("Please enter the name of the student: ")
+            stu_name = stu_name.lower()
+            for i in names:
+                if stu_name in i.lower():
+                    place = names.index(i)
+                    return place
+        # search by the student number and returns the index by subtracting 1 from the option chosen
+        elif name_v_num == "number":
+            stu_num = input("Please choose a student number. (1-4): ")
+            try:
+                stu_num = int(stu_num)
+                if 0 < stu_num < len(names) + 1:
+                    return stu_num - 1
+                else:
+                    print("Please choose an integer from 1-4 only.\n")
+                    continue
+            except ValueError:
+                print("Please choose an integer from 1-4 only.\n")
+        # lets the user that they chose an invalid entry
+        else:
+            print("Please choose a valid entry from the options given.\n")
 
 
 # validation of user choice to see favorite food, hometown of student, or neither
 def validate_choice(student_num):
-
     while True:
         choice = input("Please choose either to see the students favorite food, hometown, or neither if you would "
                        "like to move on: ")
-        if choice.lower() in "favorite food":
-            print(f"{names[student_num - 1].title()} favorite food is {favorite_food[student_num - 1].title()}")
+        choice = choice.lower()
+        if choice in "favorite food":
+            print(f"{names[student_num].title()} favorite food is {favorite_food[student_num].title()}")
             break
-        elif choice.lower() in "hometown":
-            print(f"{names[student_num - 1].title()} hometown is {hometown[student_num - 1].title()}")
+        elif choice in "hometown":
+            print(f"{names[student_num].title()} hometown is {hometown[student_num].title()}")
             break
-        elif choice.lower() == "neither":
+        elif choice == "neither":
             print("Less is more these days I guess.")
             break
         else:
             print("Please choose a valid entry as directed.")
-
-        # try:
-        #     choice = int(choice)
-        #     if choice.is_integer() and 0 < choice < 4:
-        #         return choice
-        #     else:
-        #         print("Please choose an integer from 1-3 only.")
-        # except ValueError:
-        #     print("Please choose an integer from 1-3 only.")
 
 
 # validation to break out of main loop
@@ -82,7 +91,7 @@ while True:
 
     # gets the user to choose a student from the list
     choose_student = validate_student()
-    print(names[choose_student - 1].title())
+    print(names[choose_student].title())
 
     # gets the user to choose if they want to see favorite food or hometown of the student
     validate_choice(choose_student)
